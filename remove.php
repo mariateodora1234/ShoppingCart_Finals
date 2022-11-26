578aa94bddd2de2c2e88be3dde092bdd83e853b8
mariateodora1234 <mariabiares17@gmail.com>
2022-11-25 13:52:17 -1200

    Second Commit For Remove

--- remove.php
+++ remove.php
@@ -74,7 +74,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Remove</title>
+    <title>Product Remove</title>
 </head>
 <body>
     <div class="container pt-3">
